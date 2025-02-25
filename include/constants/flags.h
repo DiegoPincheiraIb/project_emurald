0d0b4647a5fe77f4fa997671ca641b47657887fa
Javier Azócar <ignacio.azocar.carcamo@gmail.com>
2025-02-24 21:14:58 -0300

    Adds flag to skip intro (flags.h)
    Based on Issue #22

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1228,7 +1228,7 @@
 #define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
-#define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
+#define FLAG_SKIP_INTRO                                             0x499
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
 #define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
 #define FLAG_UNUSED_0x49C                                           0x49C // Unused Flag

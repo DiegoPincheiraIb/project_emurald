87987f54737870d19eeb62f27acda47d21ab46e9
DiegoPincheiraIb <diegopincheira@proton.me>
2026-07-16 22:00:15 -0400

    Adds support for RANDOM MON TEAMS for OPPOSSING TRAINERS.

---
+++ src/data/trainer_class_pools_data.h
@@ -0,0 +1,13 @@
+static const u16 sHikerSpeciesPool[] =
+{
+    SPECIES_GEODUDE,
+    SPECIES_ONIX,
+    SPECIES_MACHOP,
+};
+
+static const u16 sYoungsterSpeciesPool[] =
+{
+    SPECIES_BIDOOF,
+    SPECIES_SMOLIV,
+    SPECIES_CHIKORITA,
+};

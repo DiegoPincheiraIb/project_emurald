14993bbb80f95dbd7035b81c606b9409de7d22e5
DiegoPincheiraIb <diegopincheira@proton.me>
2026-07-17 22:27:45 -0400

    Added Eeveelutions as examples of EVO_LEVEL_HOLD_ITEM

--- src/data/pokemon/species_info/gen_1_families.h
+++ src/data/pokemon/species_info/gen_1_families.h
@@ -17845,22 +17845,14 @@ const struct SpeciesInfo gSpeciesInfoGen1[] =
         .eggMoveLearnset = sEeveeEggMoveLearnset,
         .formSpeciesIdTable = sEeveeFormSpeciesIdTable,
         .formChangeTable = sEeveeFormChangeTable,
-        .evolutions = EVOLUTION({EVO_ITEM, ITEM_THUNDER_STONE, SPECIES_JOLTEON},
-                                {EVO_ITEM, ITEM_WATER_STONE, SPECIES_VAPOREON},
-                                {EVO_ITEM, ITEM_FIRE_STONE, SPECIES_FLAREON}
-                            #if P_GEN_6_CROSS_EVOS
-                                ,{EVO_LEVEL, 0, SPECIES_SYLVEON, CONDITIONS({IF_MIN_FRIENDSHIP, FRIENDSHIP_EVO_THRESHOLD}, {IF_KNOWS_MOVE_TYPE, TYPE_FAIRY})}
-                            #endif
-                            #if P_GEN_2_CROSS_EVOS
-                                ,{EVO_LEVEL, 0, SPECIES_ESPEON, CONDITIONS({IF_MIN_FRIENDSHIP, FRIENDSHIP_EVO_THRESHOLD}, {IF_NOT_TIME, TIME_NIGHT})},
-                                {EVO_LEVEL, 0, SPECIES_UMBREON, CONDITIONS({IF_MIN_FRIENDSHIP, FRIENDSHIP_EVO_THRESHOLD}, {IF_TIME, TIME_NIGHT})}
-                            #endif
-                            #if P_GEN_4_CROSS_EVOS
-                                ,{EVO_LEVEL, 0, SPECIES_LEAFEON, CONDITIONS({IF_IN_MAP, MAP_PETALBURG_WOODS})},
-                                {EVO_ITEM, ITEM_LEAF_STONE, SPECIES_LEAFEON},
-                                {EVO_LEVEL, 0, SPECIES_GLACEON, CONDITIONS({IF_IN_MAP, MAP_SHOAL_CAVE_LOW_TIDE_ICE_ROOM})},
-                                {EVO_ITEM, ITEM_ICE_STONE, SPECIES_GLACEON}
-                            #endif
+        .evolutions = EVOLUTION({EVO_LEVEL_HOLD_ITEM, 40, SPECIES_JOLTEON,  CONDITIONS({IF_HOLD_ITEM, ITEM_MAGNET})},
+                                {EVO_LEVEL_HOLD_ITEM, 40, SPECIES_VAPOREON, CONDITIONS({IF_HOLD_ITEM, ITEM_MYSTIC_WATER})},
+                                {EVO_LEVEL_HOLD_ITEM, 40, SPECIES_FLAREON,  CONDITIONS({IF_HOLD_ITEM, ITEM_CHARCOAL})},
+                                {EVO_LEVEL_HOLD_ITEM, 40, SPECIES_SYLVEON,  CONDITIONS({IF_HOLD_ITEM, ITEM_FAIRY_FEATHER})},
+                                {EVO_LEVEL_HOLD_ITEM, 40, SPECIES_ESPEON,   CONDITIONS({IF_HOLD_ITEM, ITEM_TWISTED_SPOON})},
+                                {EVO_LEVEL_HOLD_ITEM, 40, SPECIES_UMBREON,  CONDITIONS({IF_HOLD_ITEM, ITEM_BLACK_GLASSES})},
+                                {EVO_LEVEL_HOLD_ITEM, 40, SPECIES_LEAFEON,  CONDITIONS({IF_HOLD_ITEM, ITEM_MIRACLE_SEED})},
+                                {EVO_LEVEL_HOLD_ITEM, 40, SPECIES_GLACEON,  CONDITIONS({IF_HOLD_ITEM, ITEM_NEVER_MELT_ICE})}
                             ),
     },
 
